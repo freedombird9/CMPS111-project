c099b4cf5d4f17e138664cf5c1d3b59ab8c82c22
Yongfeng Zhang <yozhang@ucsc.edu>
2014-02-14 00:38:34 -0800

    Update firstfit.c

--- project3_memory_mng/firstfit.c
+++ project3_memory_mng/firstfit.c
@@ -4,10 +4,10 @@
 
 void *ff_allot(struct handle *handlers, int handleCount, long n_bytes){
   struct fl_node *search = handlers[handleCount].freelist;
+
   while (handlers[handleCount].freelist != NULL){
     struct fl_node *node;
-    
-    if (handlers[handleCount].freelist->size >= n_bytes && !handlers[handleCount].freelist->used){
+    if (search->size >= n_bytes && !search->used){
       void *memAllocd = handlers[handleCount].freelist->blockstart;
 
       node = handlers[handleCount].freelist->blockstart + n_bytes;  /* start address of the next page */
@@ -15,17 +15,14 @@ void *ff_allot(struct handle *handlers, int handleCount, long n_bytes){
       node->used = 0;
       node->blockstart = node + sizeof(struct fl_node); /* the start address of this page's allocatable memory */
 
-      handlers[handleCount].freelist->used = 1;          /* update old page's info */
-      handlers[handleCount].freelist->size = n_bytes;
-      handlers[handleCount].freelist->next = node;
-
+      search->used = 1;          /* update old page's info */
+      search->size = n_bytes;
+      search->next = node;
 
       return memAllocd;
     }
-
+    search = search->next;
 
   }
-
-
 
 }
