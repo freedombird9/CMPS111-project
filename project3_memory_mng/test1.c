7235c839dcf7e133a9c2ba95926680c59df40d18
Yongfeng Zhang <yozhang@ucsc.edu>
2014-02-16 15:09:20 -0800

    Create test1.c

---
+++ project3_memory_mng/test1.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "math.h"
+
+int meminit(long n_bytes, unsigned int flags, int parm1);
+void* memalloc(int handle, long n_bytes);
+void memfree(void* region);
+
+int main(void)
+{
+    int handle = meminit(1024, 0x4 | 0x0 ,10);
+
+    int* q1 = memalloc(handle, 252);
+    int* q2 = memalloc(handle, 252);
+    int* q3 = memalloc(handle, 124);
+    int* q4 = memalloc(handle, 64);
+    int* q5 = memalloc(handle, 60);
+    int* q6 = memalloc(handle, 60);
+
+    memfree(q2);
+    memfree(q4);
+    memfree(q6);
+    q2 = memalloc(handle, 28);
+    q4 = memalloc(handle, 28);
+    q6 = memalloc(handle, 150);
+    
+    return 0;
+}
